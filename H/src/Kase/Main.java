5f8501c7d55f89e5405a05e74cad9a5408b1e25d
Marts Druvis Lauma <Marts Druvis Lauma@DESKTOP-BRBGMIL.lan>
2024-01-17 11:53:41 +0200

    jauna kase

---
+++ H/src/Kase/Main.java
@@ -0,0 +1,37 @@
+package Kase;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Main {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	ImageIcon icon1 = new ImageIcon("C:\\Users\\Marts Druvis Lauma\\git\\repository2\\H\\res\\Soil.png");
+	
+	Main() {
+		frame.setBounds(400, 100, 1200, 900);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		
+		panel.setLayout(null);
+		panel.setBackground(Color.cyan);
+		
+		JLabel kaste = new JLabel();
+		kaste.setBounds(100, 100, 100, 100);
+		kaste.setBackground(new Color(0f, 0f, 0f, 0f));
+		kaste.setVisible(true);
+		kaste.setOpaque(true);
+		kaste.setIcon(icon1);
+		panel.add(kaste);
+	}
+	public static void main(String[] args) {
+		new Main();
+		
+	}
+
+}
